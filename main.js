04dd30dee6cccd570002d46edffc21dc02d78e31
Devpsud <81077877+Devpsud@users.noreply.github.com>
2021-04-18 21:43:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+x = 0;
+y = 0;
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/tgV2TwXk/unnamed.png');
+}
+
+function setup() {
+   canvas = createCanvas(300,300); 
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
+        x = results[0].pose.nose.x;
+        y = results[0].pose.nose.y;
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function draw() {
+    image(video,0,0,300,300);
+    //fill(255,0,0);
+    //stroke(255,0,0);
+    //circle(x,y,20);
+    image(clown_nose,x-25,y,50,50);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
